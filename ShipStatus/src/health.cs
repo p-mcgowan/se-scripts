5ba00f780eee164da9a6cf5d233dad3a2d91396a
pat@home <mcgowanapat@gmail.com>
2020-12-12 19:30:19 +0100

    add standard methods and config options; fix bug when blocks were destroyed

--- ShipStatus/src/health.cs
+++ ShipStatus/src/health.cs
@@ -17,6 +17,12 @@ class BlockHealth {
         this.blocks = new List<IMyTerminalBlock>();
         this.damaged = new Dictionary<string, string>();
 
+        this.Reset();
+    }
+
+    public void Reset() {
+        this.Clear();
+
         if (this.program.config.Enabled("health")) {
             this.GetBlocks();
             this.RegisterTemplateVars();
@@ -26,6 +32,10 @@ class BlockHealth {
                 this.ignoreHealth = Util.Regex(System.Text.RegularExpressions.Regex.Replace(ignore, @"\s*,\s*", "|"));
             }
         }
+    }
+
+    public void Clear() {
+        this.damaged.Clear();
     }
 
     public void RegisterTemplateVars() {
@@ -45,6 +55,9 @@ class BlockHealth {
 
     public float GetHealth(IMyTerminalBlock block) {
         IMySlimBlock slimblock = block.CubeGrid.GetCubeBlock(block.Position);
+        if (slimblock == null) {
+            return 1f;
+        }
         float MaxIntegrity = slimblock.MaxIntegrity;
         float BuildIntegrity = slimblock.BuildIntegrity;
         float CurrentDamage = slimblock.CurrentDamage;
@@ -54,7 +67,8 @@ class BlockHealth {
 
     public void GetBlocks() {
         this.blocks.Clear();
-        this.program.GridTerminalSystem.GetBlocksOfType<IMyTerminalBlock>(this.blocks, b => b.IsSameConstructAs(this.program.Me));
+        this.program.GridTerminalSystem.GetBlocksOfType<IMyTerminalBlock>(this.blocks, b =>
+            this.program.config.Enabled("getAllGrids") || b.IsSameConstructAs(this.program.Me));
     }
 
     public void Refresh() {
@@ -62,6 +76,9 @@ class BlockHealth {
         bool showOnHud = this.program.config.Enabled("healthOnHud");
 
         foreach (var b in this.blocks) {
+            if (b == null) {
+                continue;
+            }
             if (this.ignoreHealth != null && this.ignoreHealth.IsMatch(b.CustomName)) {
                 continue;
             }
