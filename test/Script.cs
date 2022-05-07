aade460ef4e95aa2326646b9b20f8176da049ed4
pat@home <mcgowanapat@gmail.com>
2022-05-07 09:50:51 +0200

    test config

--- test/Script.cs
+++ test/Script.cs
@@ -1,21 +1,69 @@
-IMyShipConnector connector;
+Config config = new Config();
 
-public Program() {
-  connector = (IMyShipConnector)GridTerminalSystem.GetBlockWithName("Connector");
+public static TValue DictGet<TKey, TValue>(Dictionary<TKey, TValue> dict, TKey key, TValue defaultValue = default(TValue)) {
+    TValue value;
+    return dict.TryGetValue(key, out value) ? value : defaultValue;
 }
 
-public string GpsAt(Vector3D point, string name = ".", string colour = "") {
-    return "GPS:" + name + ":" +
-        point.X.ToString() + ":" +
-        point.Y.ToString() + ":" +
-        point.Z.ToString() + ":" + colour + ":";
+public class Config {
+    public MyIni ini;
+    public string customData;
+    public Dictionary<string, string> settings;
+    public List<MyIniKey> keys;
+
+    public Config() {
+        this.ini = new MyIni();
+        this.settings = new Dictionary<string, string>();
+        this.keys = new List<MyIniKey>();
+    }
+
+    public void Clear() {
+        this.ini.Clear();
+        this.settings.Clear();
+        this.keys.Clear();
+        this.customData = null;
+    }
+
+    public bool Parse(Program p) {
+        this.Clear();
+
+        MyIniParseResult result;
+        if (!this.ini.TryParse(p.Me.CustomData, out result)) {
+            p.Echo($"failed to parse custom data\n{result}");
+            return false;
+        }
+        this.customData = p.Me.CustomData;
+
+        string value;
+        ini.GetKeys(this.keys);
+
+        foreach (MyIniKey key in this.keys) {
+            if (ini.Get(key.Section, key.Name).TryGetString(out value)) {
+                p.Echo($"setting {key.ToString()} = {value}");
+                this.Set(key.ToString(), value);
+            }
+        }
+
+        return true;
+    }
+
+    public void Set(string name, string value) {
+        this.settings[name] = value;
+    }
+
+    public string Get(string name, string alt = null) {
+        return DictGet<string, string>(this.settings, name, null) ?? alt;
+    }
+
+    public bool Enabled(string name) {
+        return DictGet<string, string>(this.settings, name, "false") == "true";
+    }
+}
+
+public Program() {
+    config.Parse(this);
 }
 
 public void Main() {
-    // Echo($"{connector.Orientation}");
-    // Echo($"{connector.Position}");
-    Vector3D pos = connector.GetPosition();
-    Vector3D vec = new Vector3D(connector.GetPosition() + 100 * connector.WorldMatrix.Forward);
-    // get block coords and facing
-    Me.GetSurface(0).WriteText($"pos: {pos}\n{GpsAt(pos, "conn")}\nvec: {vec}\n{GpsAt(vec, "approach")}");
+
 }
