505ae04f4bd066f8afb3c3c7c35a2e2145f03d4b
p-mcgowan@winbot <winbot@patmcgowan.dev>
2022-09-18 16:39:58 +0200

    test consumption (reverse engineer auto lcd)

--- test/Script.cs
+++ test/Script.cs
@@ -1,69 +1,114 @@
-Config config = new Config();
+class Thing {
+    public MyDefinitionId electricity=new MyDefinitionId(typeof(VRage.Game.ObjectBuilders.Definitions.MyObjectBuilder_GasProperties),"Electricity");
+    public MyDefinitionId ȸ=new MyDefinitionId(typeof(VRage.Game.ObjectBuilders.Definitions.MyObjectBuilder_GasProperties),"Oxygen");
+    public MyDefinitionId ȵ=new MyDefinitionId(typeof(VRage.Game.ObjectBuilders.Definitions.MyObjectBuilder_GasProperties),"Hydrogen");
 
-public static TValue DictGet<TKey, TValue>(Dictionary<TKey, TValue> dict, TKey key, TValue defaultValue = default(TValue)) {
-    TValue value;
-    return dict.TryGetValue(key, out value) ? value : defaultValue;
-}
+    int Ɇ=0;
 
-public class Config {
-    public MyIni ini;
-    public string customData;
-    public Dictionary<string, string> settings;
-    public List<MyIniKey> keys;
-
-    public Config() {
-        this.ini = new MyIni();
-        this.settings = new Dictionary<string, string>();
-        this.keys = new List<MyIniKey>();
-    }
-
-    public void Clear() {
-        this.ini.Clear();
-        this.settings.Clear();
-        this.keys.Clear();
-        this.customData = null;
-    }
-
-    public bool Parse(Program p) {
-        this.Clear();
-
-        MyIniParseResult result;
-        if (!this.ini.TryParse(p.Me.CustomData, out result)) {
-            p.Echo($"failed to parse custom data\n{result}");
-            return false;
-        }
-        this.customData = p.Me.CustomData;
-
-        string value;
-        ini.GetKeys(this.keys);
-
-        foreach (MyIniKey key in this.keys) {
-            if (ini.Get(key.Section, key.Name).TryGetString(out value)) {
-                p.Echo($"setting {key.ToString()} = {value}");
-                this.Set(key.ToString(), value);
+    public bool Ʌ(List<IMyTerminalBlock>ł,ref double Ⱦ,ref double Ƚ,ref double ȼ,ref double Ȼ,ref double Ʉ,ref double Ƀ, bool ð){
+        if(!ð)Ɇ=0;
+        MyResourceSinkComponent Ȟ;
+        MyResourceSourceComponent ȴ;
+        for(;Ɇ<ł.Count;Ɇ++){
+            if(ł[Ɇ].Components.TryGet<MyResourceSinkComponent>(out Ȟ)){
+                Ⱦ+=Ȟ.CurrentInputByType(electricity);
+                Ƚ+=Ȟ.MaxRequiredInputByType(electricity);
+            }if(ł[Ɇ].Components.TryGet<MyResourceSourceComponent>(out ȴ)){
+                ȼ+=ȴ.CurrentOutputByType(electricity);
+                Ȼ+=ȴ.MaxOutputByType(electricity);
             }
+            IMyBatteryBlock ɂ=(ł[Ɇ] as IMyBatteryBlock);
+            Ʉ+=ɂ.CurrentStoredPower;
+            Ƀ+=ɂ.MaxStoredPower;
         }
-
         return true;
     }
 
-    public void Set(string name, string value) {
-        this.settings[name] = value;
+    int Ɂ=0;
+    public bool ɀ(List<IMyTerminalBlock>ł,MyDefinitionId ȿ,ref double Ⱦ,ref double Ƚ,ref double ȼ,ref double Ȼ,bool ð){
+        if(!ð)Ɂ=0;
+        MyResourceSinkComponent Ȟ;
+        MyResourceSourceComponent ȴ;
+        for(;Ɂ<ł.Count;Ɂ++){
+            if(ł[Ɂ].Components.TryGet<MyResourceSinkComponent>(out Ȟ)){
+                Ⱦ+=Ȟ.CurrentInputByType(ȿ);
+                Ƚ+=Ȟ.MaxRequiredInputByType(ȿ);
+            }
+            if(ł[Ɂ].Components.TryGet<MyResourceSourceComponent>(out ȴ)){
+                ȼ+=ȴ.CurrentOutputByType(ȿ);
+                Ȼ+=ȴ.MaxOutputByType(ȿ);
+            }
+        }
+        return true;
     }
 
-    public string Get(string name, string alt = null) {
-        return DictGet<string, string>(this.settings, name, null) ?? alt;
-    }
-
-    public bool Enabled(string name) {
-        return DictGet<string, string>(this.settings, name, "false") == "true";
+    int ȣ=0;
+    public bool ȑ(List<IMyTerminalBlock>ł,string ȡ,ref double Ƞ,ref double ȟ,bool ð){
+        if(!ð){ȣ=0;ȟ=0;Ƞ=0;}
+        MyResourceSinkComponent Ȟ;
+        for(;ȣ<ł.Count;ȣ++){
+            IMyGasTank Ň=ł[ȣ] as IMyGasTank;
+            if(Ň==null)continue;
+            double ȝ=0;
+            if(Ň.Components.TryGet<MyResourceSinkComponent>(out Ȟ)){
+                ListReader<MyDefinitionId>Ȝ=Ȟ.AcceptedResources;
+                int Y=0;
+                for(;Y<Ȝ.Count;Y++){
+                    if(string.Compare(Ȝ[Y].SubtypeId.ToString(),ȡ,true)==0){
+                        ȝ=Ň.Capacity;
+                        ȟ+=ȝ;Ƞ+=ȝ*Ň.FilledRatio;break;
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
 
-public Program() {
-    config.Parse(this);
-}
+public MyDefinitionId electricity = new MyDefinitionId(typeof(VRage.Game.ObjectBuilders.Definitions.MyObjectBuilder_GasProperties), "Electricity");
+public MyDefinitionId oxygen = new MyDefinitionId(typeof(VRage.Game.ObjectBuilders.Definitions.MyObjectBuilder_GasProperties), "Oxygen");
+public MyDefinitionId hydrogen = new MyDefinitionId(typeof(VRage.Game.ObjectBuilders.Definitions.MyObjectBuilder_GasProperties), "Hydrogen");
+MyResourceSinkComponent resourceSink;
+Dictionary<string, float> myDict = new Dictionary<string, float>();
+
+
 
 public void Main() {
+    List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
 
+    // get blocks
+    MyResourceSinkComponent resourceSink;
+    MyResourceSourceComponent resourceSource;
+
+    GridTerminalSystem.GetBlocksOfType<IMyTerminalBlock>(blocks);
+
+
+    Dictionary<string, float> myDict = new Dictionary<string, float>();
+
+    int i = 0;
+    foreach (var block in blocks) {
+        if (block.Components == null) {
+            continue;
+        }
+        if (block.Components.TryGet<MyResourceSinkComponent>(out resourceSink)) {
+            myDict.Add($"{block.CustomName} {i++}", resourceSink.CurrentInputByType(electricity));
+            // Echo($"{block.CustomName}");
+            // Echo($"  CurrentInputByType: {resourceSink.CurrentInputByType(electricity)}");
+            // Echo($"  MaxRequiredInputByType: {resourceSink.MaxRequiredInputByType(electricity)}");
+        }
+        // if (block.Components.TryGet<MyResourceSourceComponent>(out resourceSource)) {
+        //     Echo("  sources:");
+        //     Echo($"  DefinedOutputByType: {resourceSource.DefinedOutputByType(electricity)}");
+        //     Echo($"  CurrentOutputByType: {resourceSource.CurrentOutputByType(electricity)}");
+        //     Echo($"  MaxOutputByType: {resourceSource.MaxOutputByType(electricity)}");
+        // } else {
+        //     Echo("  No source");
+        // }
+    }
+    // myDict = from entry in myDict orderby entry.Value descending select entry;
+    myDict = myDict.OrderBy(x => -x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+    foreach (var item in myDict) {
+        Echo($"{item.Key}: {item.Value}");
+    }
 }
